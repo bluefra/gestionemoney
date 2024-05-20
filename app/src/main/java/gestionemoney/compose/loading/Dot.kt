c948d9bbab3c5d08b4d9b9e98be7810edea40cd3
Karma39 <matteocampagnaro3@gmail.com>
2024-05-20 17:38:35 +0200

    final fixes for the revert

--- app/src/main/java/gestionemoney/compose/loading/Dot.kt
+++ app/src/main/java/gestionemoney/compose/loading/Dot.kt
@@ -8,15 +8,16 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 
     @Composable
-    fun Dot(offset: Float) {
+    fun Dot(offset: Float, color: Color) {
         Spacer(
             Modifier
                 .size(dotSize)
                 .offset(y = -offset.dp)
                 .background(
-                    color = dotColor,
+                    color = color,
                     shape = CircleShape
                 )
         )
